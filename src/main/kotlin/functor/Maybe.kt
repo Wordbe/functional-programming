5bb1fc6ebdc1fa1c9c9fbea0b93877fbacd91035
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-03-14 16:01:25 +0900

    feat: applicative functor with extension

--- src/main/kotlin/functor/Maybe.kt
+++ src/main/kotlin/functor/Maybe.kt
@@ -4,6 +4,8 @@ sealed class Maybe<out A> : Functor<A> {
     abstract override fun toString(): String
 
     abstract override fun <B> fmap(f: (A) -> B): Maybe<B>
+
+    companion object
 }
 
 data class Just<out A>(val value: A) : Maybe<A>() {
@@ -16,4 +18,11 @@ object Nothing : Maybe<kotlin.Nothing>() {
     override fun toString(): String = "Nothing"
 
     override fun <B> fmap(f: (kotlin.Nothing) -> B): Maybe<B> = Nothing
+}
+
+fun <A> Maybe.Companion.pure(value: A) = Just(value)
+
+infix fun <A, B> Maybe<(A) -> B>.apply(f: Maybe<A>): Maybe<B> = when (this) {
+    is Just -> f.fmap(this.value)
+    is Nothing -> Nothing
 }
