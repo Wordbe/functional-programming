5d4131906e3e01380aceacd1a784ead59f8f7f78
Seongho Jin <40506736+Wordbe@users.noreply.github.com>
2022-03-17 17:22:32 +0900

    feat: functional stream

---
+++ src/main/kotlin/collection/FunStream.kt
@@ -0,0 +1,16 @@
+package collection
+
+sealed class FunStream<out T> {
+    object Nil : FunStream<Nothing>()
+    data class Cons<out T>(val head: () -> T, val tail: () -> FunStream<T>) : FunStream<T>()
+}
+
+fun <T> FunStream<T>.getHead(): T = when (this) {
+    FunStream.Nil -> throw NoSuchElementException()
+    is FunStream.Cons -> head()
+}
+
+fun <T> FunStream<T>.getTail(): FunStream<T> = when (this) {
+    FunStream.Nil -> throw NoSuchElementException()
+    is FunStream.Cons -> tail()
+}
